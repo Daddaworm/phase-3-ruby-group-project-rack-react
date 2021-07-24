1f7b1da6c0908929b22dffb600438b1b0c59ae91
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-07-24 17:31:53 -0400

    Added seed data

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -1,17 +1,52 @@
+require 'pry'
 class Application
+
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/test/) 
-      return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
-    elsif req.path.match(/owner/) 
-      return [200, { 'Content-Type' => 'application/json' }, [ {data: Owner.all}.to_json ]]
-    elsif req.path.match(/vehicle/)
-      return [200, { 'Content-Type' => 'application/json' }, [ {data: Vehicle.all}.to_json ]]
-    elsif req.path.match(/routine/)
+    # if req.path.match(/test/) 
+    #   return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
+
+    if req.path.match(/owners/) && req.post?
+      data = JSON.parse(req.body.read) #code to unpackage the stringifiy JSON
+      userExists = Owner.find_by(username: data["username"]) #check if user xitst with that username? usernames must be uniq
+      if userExists
+        return [200, { 'Content-Type' => 'application/json' }, [ {:error => "User with username: #{userExists.username} already exists"}.to_json ]]   
+      else
+        owner = Owner.create(data)
+    
+        # What do i want to send in response?
+        return [200, {'Content-Type' => 'application/json'}, [{
+          :message => "You successfullly signed up for Vehicle Maintenance App",
+          :owner => owner
+          }.to_json]]
+      end
+
+    elsif req.delete?
+      # binding.pry
+
+      id = req.path.split("/vehicles/").last
+      Vehicle.find(id).delete
+      return [200, {'Content-Type' => 'application/json'}, [{:message => "You successfullly signed up for Vehicle Maintenance App",}.to_json]]
+
+
+
+    elsif req.path.match(/vehicles/)
+      username = req.params["q"]
+      user = User.find_by(:username => username)
+      # return [200, { "Content-Type" => "application/json" }, [{ :data => {:user => user, :userContacts => user.contacts}}.to_json]]
+
+      # return [200, { 'Content-Type' => 'application/json' }, [ {data: Vehicle.all}.to_json ]]
+    elsif req.path.match(/routines/)
       return [200, { 'Content-Type' => 'application/json' }, [ {data: Routine.all}.to_json ]]
+
+    elsif req.path.match(/owners/)
+      username = req.params["q"]
+      owner = Owner.find_by(:username => username)
+      return [200, { "Content-Type" => "application/json" }, [{:owner => owner, :ownerVehicles => owner.vehicles}.to_json]]
+      # return [200, { 'Content-Type' => 'application/json' }, [ {data: Owner.all}.to_json ]]
     else
       resp.write "Path Not Found"
     end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,12 +1,24 @@
+require 'faker'
 
+puts "Deleting previous data"
+
+Vehicle.destroy_all
+Owner.destroy_all
+Routine.destroy_all
+
+puts "Creating owner and vehicles"
+
+Owner.create(username: "Ethan", password: "1234") 
 
 5.times do
 Owner.create(username: Faker::Name.name, password: "1234") 
 end
 
+20.times do
+Vehicle.create(year: Faker::Vehicle.year, make: Faker::Vehicle.make , model: Faker::Vehicle.model, vin: Faker::Vehicle.vin, mileage: Faker::Vehicle.mileage, oil_type: "10W-40", oil_filter: "KN-5", owner_id: rand(1..11))
+end
 
-Vehicle.create(year: 2014, make: "Ram", model: "1500", vin: "4RD43-RR56-2231-X5R5T", mileage: 74403, oil_type: "10W-40", oil_filter: "KN-5")
-Vehicle.create(year: 2016, make: "Nissan", model: "Altima", vin: "54343-RR56-2RF1-HG556", mileage: 55043, oil_type: "5W-20", oil_filter: "MP-22")
+# Vehicle.create(year: 2013, make: "Ram", model: "1500", vin: "54343-RR56-2RF1-HG556", mileage: 55043, oil_type: "5W-20", oil_filter: "MP-22")
 
 
 Routine.create
