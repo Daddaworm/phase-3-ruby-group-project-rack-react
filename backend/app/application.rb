dae5969e36e597d05e5c63048d7b5d72c73004b4
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-08-01 23:24:49 -0400

    Added new resources

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -14,37 +14,41 @@ class Application
         return [200, { 'Content-Type' => 'application/json' }, [ {:error => "User with username: #{userExists.username} already exists"}.to_json ]]   
       else
         owner = Owner.create(data)
-    
-        # What do i want to send in response?
         return [200, {'Content-Type' => 'application/json'}, [{
           :message => "You successfullly signed up for Vehicle Maintenance App",
           :owner => owner
           }.to_json]]
       end
 
-    elsif req.delete?cd
-      # binding.pry
-
-      id = req.path.split("/vehicles/").last
-      Vehicle.find(id).delete
-      return [200, {'Content-Type' => 'application/json'}, [{:message => "You successfullly signed up for Vehicle Maintenance App",}.to_json]]
-
-
-
-    elsif req.path.match(/vehicles/)
-      username = req.params["q"]
-      user = User.find_by(:username => username)
-      # return [200, { "Content-Type" => "application/json" }, [{ :data => {:user => user, :userContacts => user.contacts}}.to_json]]
-
-      # return [200, { 'Content-Type' => 'application/json' }, [ {data: Vehicle.all}.to_json ]]
-    elsif req.path.match(/routines/)
-      return [200, { 'Content-Type' => 'application/json' }, [ {data: Routine.all}.to_json ]]
-
     elsif req.path.match(/owners/)
+      # binding.pry
       username = req.params["q"]
       owner = Owner.find_by(:username => username)
       return [200, { "Content-Type" => "application/json" }, [{:owner => owner, :ownerVehicles => owner.vehicles}.to_json]]
-      # return [200, { 'Content-Type' => 'application/json' }, [ {data: Owner.all}.to_json ]]
+
+    elsif req.path.match(/vehicles/)
+      vehicles = Vehicle.all
+      return [200, { "Content-Type" => "application/json" }, [{:vehicles => vehicles}.to_json]]
+
+    elsif req.delete?
+      id = req.path.split("/vehicles/").last
+      Vehicle.find(id).delete
+      return [200, {'Content-Type' => 'application/json'}, [{:message => "You successfullly signed up for Vehicle Maintenance App",}.to_json]]
+    elsif req.path.match(/vehicles/) && req.post?
+      data = JSON.parse(req.body.read)
+      vehicleExists = Vehicle.find_by(vin: data["vin"])
+      
+      if vehicleExists
+        return [200, { 'Content-Type' => 'application/json' }, [ {:error => "Vehilce with vin: #{vehicleExists.vin} already exists"}.to_json ]] 
+      else
+        vehicle = Vehicle.create(data)
+        # binding.pry
+        return [200, {'Content-Type' => 'application/json'}, [{
+          :message => "You successfullly created a vehicle profile",
+          :vehicle => vehicle
+          }.to_json]]
+      end
+    
     else
       resp.write "Path Not Found"
     end
--- backend/app/models/owner.rb
+++ backend/app/models/owner.rb
@@ -1,4 +1,4 @@
 class Owner < ActiveRecord::Base
     has_many :vehicles
-    # has_many :routines, through: :vehicles
+    
 end
