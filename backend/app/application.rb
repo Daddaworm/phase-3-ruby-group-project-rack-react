d1b86ace2415727dce107a9bf6fceb56b10eda38
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-08-02 01:10:53 -0400

    Updated routes

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -20,34 +20,58 @@ class Application
           }.to_json]]
       end
 
+    elsif req.path.match(/vehicles/) && req.post?
+      data = JSON.parse(req.body.read) #code to unpackage the stringifiy JSON
+      vehicleExists = Vehicle.find_by(vin: data["vin"]) #check if user xitst with that username? usernames must be uniq
+      if vehicleExists
+        return [200, { 'Content-Type' => 'application/json' }, [ {:error => "Vehicle with vin: #{vehicleExists.vin} already exists"}.to_json ]]   
+      else
+        vehicle = Vehicle.create(data)
+        return [200, {'Content-Type' => 'application/json'}, [{
+          :message => "Vehicle created!",
+          :vehicle => vehicle
+          }.to_json]]
+      end
+
     elsif req.path.match(/owners/)
       # binding.pry
       username = req.params["q"]
       owner = Owner.find_by(:username => username)
       return [200, { "Content-Type" => "application/json" }, [{:owner => owner, :ownerVehicles => owner.vehicles}.to_json]]
 
+    elsif req.delete?
+
+      id = req.path.split("/vehicles/").last
+
+      vehicle = Vehicle.find(id)
+      vehicle.destroy
+
+      return [200, {'Content-Type' => 'application/json'}, [{:message => "You successfullly signed up for Vehicle Maintenance App",}.to_json]]
+
     elsif req.path.match(/vehicles/)
       vehicles = Vehicle.all
       return [200, { "Content-Type" => "application/json" }, [{:vehicles => vehicles}.to_json]]
 
-    elsif req.delete?
-      id = req.path.split("/vehicles/").last
-      Vehicle.find(id).delete
-      return [200, {'Content-Type' => 'application/json'}, [{:message => "You successfullly signed up for Vehicle Maintenance App",}.to_json]]
-    elsif req.path.match(/vehicles/) && req.post?
-      data = JSON.parse(req.body.read)
-      vehicleExists = Vehicle.find_by(vin: data["vin"])
-      
-      if vehicleExists
-        return [200, { 'Content-Type' => 'application/json' }, [ {:error => "Vehilce with vin: #{vehicleExists.vin} already exists"}.to_json ]] 
-      else
-        vehicle = Vehicle.create(data)
-        # binding.pry
-        return [200, {'Content-Type' => 'application/json'}, [{
-          :message => "You successfullly created a vehicle profile",
-          :vehicle => vehicle
-          }.to_json]]
-      end
+    # elsif req.delete?
+    #   id = req.path.split("/vehicles/").last
+    #   binding.pry
+    #   Vehicle.find(id).destroy
+    #   return [200, {'Content-Type' => 'application/json'}, [{:message => "You successfullly signed up for Vehicle Maintenance App",}.to_json]]
+
+    # elsif req.path.match(/vehicles/) && req.post?
+    #   binding.pry
+    #   data = JSON.parse(req.body.read)
+    #   vehicleExists = Vehicle.find_by(vin: data["vin"])
+    #   if vehicleExists
+    #     return [200, { 'Content-Type' => 'application/json' }, [ {:error => "Vehilce with vin: #{vehicleExists.vin} already exists"}.to_json ]] 
+    #   else
+    #     vehicle = Vehicle.create(data)
+    #     binding.pry
+    #     return [200, {'Content-Type' => 'application/json'}, [{
+    #       :message => "You successfullly created a vehicle profile",
+    #       :vehicle => vehicle
+    #       }.to_json]]
+    #   end
     
     else
       resp.write "Path Not Found"
